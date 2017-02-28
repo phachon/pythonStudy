8629ac277286654ffa43f749dd150dcfddd587cf
phachon <phachon@163.com>
2017-02-28 22:14:24 +0800

    完成转换脚步

--- railway/plf_to_matrix.py
+++ railway/plf_to_matrix.py
@@ -14,7 +14,7 @@ runLog = Logger('logs/run.log')
 errorLog = Logger('logs/error.log')
 
 
-def read_data(file):
+def read_data(file, save_file):
 	""" 读取plf数据 """
 
 	if os.path.isfile(file) is False:
@@ -28,12 +28,14 @@ def read_data(file):
 	# 总行数
 	rowNumbers = sheet.nrows
 
-	data = []
 	train_numbers = []
+	runTimes = []
 
 	for i in range(rowNumbers):
+		print("第 "+i+" 行开始")
 		rowValue = sheet.row_values(i)
-
+		if len(rowValue) is 0:
+			continue
 		if rowValue[0] == '车次':
 			continue
 		train_number = rowValue[0]
@@ -43,11 +45,30 @@ def read_data(file):
 		if train_number not in train_numbers:
 			train_numbers.append(train_number)
 
-	print(train_numbers)
-		#print(runTime)
-		#print(plf)
-	exit()
+		if runTime not in runTimes:
+			runTimes.append(runTime)
 
+		row = train_numbers.index(train_number) + 1
+		line = runTimes.index(runTime) + 1
+
+		write_file(line, row, plf, save_file)
+
+	for x in range(len(runTimes)):
+		write_file(x+1, 0, runTimes[x], save_file)
+	for y in range(len(train_numbers)):
+		write_file(0, y+1, train_numbers[y], save_file)
+
+	print(len(train_numbers))
+
+
+def write_file(line, row, value, file='plf_matrix.xls'):
+	""" 安坐标写入 """
+
+	old_data = xlrd.open_workbook(file)
+	write = copy(old_data)
+	write_sheet = write.get_sheet(0)
+	write_sheet.write(line, row, value)
+	write.save(file)
 
 if __name__ == '__main__':
 
@@ -61,5 +82,5 @@ if __name__ == '__main__':
 		write.save(save_file)
 		runLog.info('create file ' + save_file + ' success!')
 
-	result = read_data(old_file)
+	read_data(old_file, save_file)
 
